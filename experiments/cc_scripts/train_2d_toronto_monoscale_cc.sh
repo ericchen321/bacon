156621c0deb475036886ad05ce0997036c84b26e
eric <cgx1997@outlook.com>
2022-05-09 12:14:35 -0700

    added config and cc train scripts for 12 new images

---
+++ experiments/cc_scripts/train_2d_toronto_monoscale_cc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --account=def-rhodin
+#SBATCH --job-name=trainTorontoBacon
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=24G
+module load python/3.8
+module load StdEnv/2020
+module load cuda/11.1.1
+
+cd /home/gxc321/
+source BaconEnv/bin/activate
+cd /home/gxc321/scratch/bacon/experiments/
+python train_img.py --config ./config/img/bacon_toronto_monoscale.ini
