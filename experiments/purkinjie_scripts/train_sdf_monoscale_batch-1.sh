49fe2d7d160876b6b3f14ae161570b04c6a2276d
eric <cgx1997@outlook.com>
2022-05-10 01:56:40 -0700

    added scripts to train on purkinje

---
+++ experiments/purkinjie_scripts/train_sdf_monoscale_batch-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python train_sdf.py --config ./config/sdf/bacon_camera_monoscale.ini
+python train_sdf.py --config ./config/sdf/bacon_david_monoscale.ini
+python train_sdf.py --config ./config/sdf/bacon_dragon_monoscale.ini
+python train_sdf.py --config ./config/sdf/bacon_dragon_warrior_monoscale.ini
+python train_sdf.py --config ./config/sdf/bacon_engine_monoscale.ini
