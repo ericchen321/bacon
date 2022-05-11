44fed768e94f8738451789687c89ebc9a48d82ee
eric <cgx1997@outlook.com>
2022-05-11 14:48:08 -0700

    added local multiscale training script

---
+++ experiments/purkinjie_scripts/train_sdf_multiscale_batch-0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python train_sdf.py --config ./config/sdf/bacon_airplane_multiscale.ini
+#python train_sdf.py --config ./config/sdf/bacon_armadillo_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_asian_dragon_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_beard_man_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_bimba_multiscale.ini
---
+++ experiments/purkinjie_scripts/train_sdf_multiscale_batch-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python train_sdf.py --config ./config/sdf/bacon_guangong_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_lion_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_motorbike_multiscale.ini
+python train_sdf.py --config ./config/sdf/bacon_napoleon_multiscale.ini
+#python train_sdf.py --config ./config/sdf/bacon_thai_statue_multiscale.ini
