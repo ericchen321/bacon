ffb008b066fabfdb8111b7005a4e73761a26808a
eric <cgx1997@outlook.com>
2022-05-11 15:32:04 -0700

    fixed / added multiscale train scripts for cc

--- experiments/cc_scripts/train_2d_hills_multiscale_cc.sh
+++ experiments/cc_scripts/train_2d_hills_multiscale_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --account=def-rhodin
-#SBATCH --job-name=trainDogBacon
+#SBATCH --job-name=trainHillsBacon
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=24G
 module load python/3.8
@@ -11,4 +11,4 @@ module load cuda/11.1.1
 cd /home/gxc321/
 source BaconEnv/bin/activate
 cd /home/gxc321/scratch/bacon/experiments/
-python train_img.py --config ./config/img/bacon_dog_multiscale.ini
+python train_img.py --config ./config/img/bacon_hills_multiscale.ini
