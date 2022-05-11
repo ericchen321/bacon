b5c7441e1a515867f66cd2445bfec2374c199c85
eric <cgx1997@outlook.com>
2022-05-11 14:47:42 -0700

    added multiscale rendering configs and train scripts

---
+++ experiments/purkinjie_scripts/render_sdf_multiscale.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python render_sdf.py -c render_sdf_config/bacon_airplane_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_armadillo_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_asian_dragon_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_beard_man_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_bimba_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_camera_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_david_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_dragon_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_dragon_warrior_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_engine_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_guangong_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_lion_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_motorbike_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_napoleon_monoscale.ini
+python render_sdf.py -c render_sdf_config/bacon_thai_statue_monoscale.ini
