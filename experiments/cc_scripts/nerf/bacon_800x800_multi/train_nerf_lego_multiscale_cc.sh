6fc10d35a8eeb782ed0524e6256c041d2f8f5591
eric <cgx1997@outlook.com>
2022-07-08 16:32:13 -0700

    restored batch sizes; added configs/train scripts for 800x800 dataset

--- experiments/cc_scripts/nerf/bacon_800x800_multi/train_nerf_lego_multiscale_cc.sh
+++ experiments/cc_scripts/nerf/bacon_800x800_multi/train_nerf_lego_multiscale_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --account=def-rhodin
-#SBATCH --job-name=tr_lego_default_multi_bacon
+#SBATCH --job-name=tr_lego_800x800_multi_bacon
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=24G
 module load python/3.8
@@ -11,4 +11,4 @@ module load cuda/11.1.1
 cd /home/gxc321/
 source BaconEnv/bin/activate
 cd /home/gxc321/scratch/bacon/
-source experiments/train_nerf.sh lego blender bacon_default_multi 512
+source experiments/train_nerf.sh lego blender bacon_default_multi 800
