0b091b52b59c96b953d6fd52d9f99a85531e27a1
typan <sampty@student.ubc.ca>
2022-07-09 16:53:38 -0700

    add scripts for nerf_bacon and 3d

---
+++ experiments/cc_scripts/nerf/bacon_default_multi/train_nerf_mic_multiscale_cc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=120:00:00
+#SBATCH --account=def-rhodin
+#SBATCH --job-name=tr_mic_default_multi_bacon
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=24G
+module load python/3.8
+module load StdEnv/2020
+module load cuda/11.1.1
+
+cd /home/gxc321/
+source BaconEnv/bin/activate
+cd /home/gxc321/scratch/bacon/
+source experiments/train_nerf.sh mic blender bacon_default_multi 512
