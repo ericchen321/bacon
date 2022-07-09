6c440c7aff0828a6ebf9799edb8b6a3541b025ad
eric <cgx1997@outlook.com>
2022-07-09 14:07:45 -0700

    updated CC training time and job name

--- experiments/cc_scripts/sdf/bacon_default_multi/train_3d_thai_statue_multiscale_cc.sh
+++ experiments/cc_scripts/sdf/bacon_default_multi/train_3d_thai_statue_multiscale_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --account=def-rhodin
-#SBATCH --job-name=tr_thai_statue_multi_bacon
+#SBATCH --job-name=tr_thai_statue_default_multi_bacon
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=24G
 module load python/3.8
