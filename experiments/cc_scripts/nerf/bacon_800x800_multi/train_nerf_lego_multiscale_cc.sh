6b0825dac80e716a11e5b85e943afc6203319e0e
eric <cgx1997@outlook.com>
2022-07-08 19:15:14 -0700

    request 64G mem for 800x800 exps

--- experiments/cc_scripts/nerf/bacon_800x800_multi/train_nerf_lego_multiscale_cc.sh
+++ experiments/cc_scripts/nerf/bacon_800x800_multi/train_nerf_lego_multiscale_cc.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=def-rhodin
 #SBATCH --job-name=tr_lego_800x800_multi_bacon
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --mem=24G
+#SBATCH --mem=64G
 module load python/3.8
 module load StdEnv/2020
 module load cuda/11.1.1
