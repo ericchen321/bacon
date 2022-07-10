01b1e97b759139ea6d819313779ed4d2eefab72e
eric <cgx1997@outlook.com>
2022-07-10 13:42:38 -0700

    removed local training script; refactored SDF rendering script

--- experiments/eval_sdf.sh
+++ experiments/eval_sdf.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 SHAPE_NAME="$1"
-BASE_CONFIG_NAME="$2"
-RESOLUTION="$3"
+LOGGING_SUBDIR="$2"
+BASE_CONFIG_NAME="$3"
+RESOLUTION="$4"
 
 # define paths
 BASE_CONFIG_PATH="experiments/config/sdf_render/${BASE_CONFIG_NAME}.ini"
-LOG_DIR="logs"
+LOG_DIR="logs/$LOGGING_SUBDIR/"
 
 # overwrite base configs
 arg_string="--config ${BASE_CONFIG_PATH} \
+--logging_root $LOG_DIR \
 --experiment_name ${BASE_CONFIG_NAME}_${SHAPE_NAME} \
 --ckpt ${LOG_DIR}/${BASE_CONFIG_NAME}_${SHAPE_NAME}/checkpoints/model_final.pth \
 "
---
+++ experiments/purkinjie_scripts/sdf/render_sdf_jun28.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exp_category="$1"
+logging_subdir="$2"
+resolution="$3"
+
+declare -a shape_names=(
+    "at-ot"
+    "cathedral"
+    "gear_shift"
+    "lunar_lander"
+    "ninjago_city"
+    "oak_tree"
+    )
+
+for shape_name in ${shape_names[@]}; do
+    source experiments/eval_sdf.sh $shape_name $logging_subdir $exp_category $resolution
+done
